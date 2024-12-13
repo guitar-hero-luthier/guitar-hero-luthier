3ffde30f4e21a63b05045db63b7615c676502d15
joaovs2004 <joaovitorschmittschmitt@gmail.com>
2024-12-12 23:08:54 -0300

    adição de comandos

--- socket/client.js
+++ socket/client.js
@@ -18,6 +18,11 @@ const token = jwt.sign(message, secretKey, { expiresIn: exp });
 
 const bot_telegram = new Telegraf(process.env.BOT_TOKEN);
 
+const fichas = {
+  "fichas_compradas": 0,
+  "fichas_ativas": 0
+};
+
 const socket = io("wss://feira-de-jogos.dev.br/arcade", {
   path: "/api/v2/machine",
   transports: ['websocket'],
@@ -42,11 +47,14 @@ socket.on("coinInsert", (data) => {
 
       bot_telegram.telegram.sendMessage(process.env.ID_TELEGRAM_JOAOS, "Ficha comprada!");
       bot_telegram.telegram.sendMessage(process.env.ID_TELEGRAM_JOAOP, "Ficha comprada!");
+
+      fichas.fichas_compradas += 1;
+      fichas.fichas_ativas += 1;
     }
   } catch (error) {
     console.error("Erro ao processar dados de moeda:", error);
   }
-})
+});
 
 socket.on("connect_error", (error) => {
   console.log("Erro de conexão: ");
@@ -56,6 +64,34 @@ socket.on("connect_error", (error) => {
 socket.on("error", (error) => {
   console.log("Erro: ");
   console.log(error);
+});
+
+bot_telegram.command("listar", (ctx) => {
+  ctx.reply(`Fichas compradas ao todo: ${fichas.fichas_compradas}`);
+});
+
+bot_telegram.command("listar_ativas", (ctx) => {
+  ctx.reply(`Fichas ativas atualmente: ${fichas.fichas_ativas}`);
+});
+
+bot_telegram.command("desativar", (ctx) => {
+  ctx.reply("Quantas fichas deletar?");
+
+  bot_telegram.on("text", (ctx) => {
+    try {
+      const fichas_deletar = Number(ctx.message.text.trim());
+      
+      if (fichas_deletar <= fichas.fichas_ativas) {
+        fichas.fichas_ativas -= fichas_deletar;
+
+        ctx.reply("Fichas deletadas com sucesso!");
+      } else {
+        ctx.reply("Número maior que a quantidade de fichas ativas atualmente");
+      }
+    } catch (error) {
+      ctx.reply("Falha ao deletar fichas");
+    }
+  });
 });
 
 bot_telegram.launch();
